abb952eb86a68db0196ca01111eba974d72e3d59
Arun <70346491+chaudhary-arun@users.noreply.github.com>
2020-12-06 23:47:48 +0530

    Add files via upload

--- apiQuotes.js
+++ apiQuotes.js
@@ -20,6 +20,31 @@ apiQuotes.get('/',(req,res,next)=>{
         res.send({quotes:quotes});
 });
 
+apiQuotes.delete('/',(req,res,next)=>{
+    if(req.query.person !== undefined){
+        var personFound = false;
+         for(var index = 0; index < quotes.length; index++) {
+             var foundQuote = quotes[index];
+             if(foundQuote.person == req.query.person) {
+                 console.log('Removed Quote: ',  foundQuote.quote); 
+                 
+                 quotes.splice(index, 1);
+                 personFound = true;
+                 break;
+             }
+         }
+         if(!personFound)
+            res.send({
+            deletedPerson: "Author doesn't Exist"
+             });
+         else
+             res.send({
+             deletedPerson : req.query.person
+         });
+    }
+    else    
+        res.status(404).send();
+})
 apiQuotes.post('/',(req,res,next)=>{
     if(req.query.person!== undefined && req.query.quote !==undefined){
         quotes.push({
